d8d9c33ff28a582d4d1e7cc1319715e939bbc322
Gleb Gorev <gorev.gb@outlook.com>
2023-02-24 21:50:32 +0400

    FED-30 Connect API with WebUI

---
+++ fed.cloud.communication/Recipe/RecipeIngredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace fed.cloud.communication.Recipe
+{
+    public class RecipeIngredient
+    {
+        public Guid Id { get; set; }
+        
+        public string IngredientName { get; set; }
+        
+        public int ReferenceNumber { get; set; }
+        
+        public double Quantity { get; set; }
+        
+        public int Unit { get; set; }
+    }
+}
